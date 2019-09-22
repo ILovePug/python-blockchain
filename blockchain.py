7dc899c22ab6a6a6daf7cd88e27b55b84a66643b
ILovePug <wengqians@yahoo.com>
2019-09-22 05:35:36 -0400

    add file saving

--- blockchain.py
+++ blockchain.py
@@ -1,6 +1,6 @@
 import functools
 from collections import OrderedDict
-
+import json
 from hash_util import hash_string_256, hash_block
 
 MINING_REWARD = 10
@@ -14,6 +14,51 @@ blockchain = [genesis_block]
 open_transactions = []
 owner = 'Qian'
 participants = {'Qian'}
+
+
+def load_data():
+    global blockchain
+    global open_transactions
+    global genesis_block
+    try:
+        with open('blockchain.txt', mode='r') as f:
+            file_content = f.readlines()
+
+            blockchain = json.loads(file_content[0][:-1])
+            updated_blockchain = []
+            for block in blockchain:
+                updated_block = {
+                    'previous_hash': block['previous_hash'],
+                    'index': block['index'],
+                    'proof': block['proof'],
+                    'transactions': [OrderedDict(
+                        [('sender', tx['sender']), ('recipient', tx['recipient']), ('amount', tx['amount'])]) for tx in block['transactions']]
+                }
+                updated_blockchain.append(updated_block)
+            blockchain = updated_blockchain
+            open_transactions = json.loads(file_content[1])
+            updated_transactions = []
+            for tx in open_transactions:
+                updated_transaction = OrderedDict(
+                    [('sender', tx['sender']), ('recipient', tx['recipient']), ('amount', tx['amount'])])
+                updated_transactions.append(updated_transaction)
+            open_transactions = updated_transactions
+    except (IOError, IndexError): 
+        blockchain = [genesis_block]
+        open_transactions = []
+
+load_data()
+
+
+def save_data():
+    try:
+        with open('blockchain.txt', mode='w') as f:
+            f.write(json.dumps(blockchain))
+            f.write('\n')
+            f.writable(json.dumps(open_transactions))
+
+    except IOError:
+        print('saving failed')
 
 
 def valid_proof(transactions, last_hash, proof):
@@ -71,11 +116,13 @@ def add_transaction(recipient, sender=owner, amount=1.0):
         :recipient: the recipient
         :amount: the amount
     """
-    transaction = OrderedDict([('sender', sender),('recipient',recipient),('amount',amount)])
+    transaction = OrderedDict(
+        [('sender', sender), ('recipient', recipient), ('amount', amount)])
     if verify_transaction(transaction):
         open_transactions.append(transaction)
         participants.add(recipient)
         participants.add(sender)
+        save_data()
         return True
     return False
 
@@ -92,7 +139,7 @@ def mine_block():
     #     'amount': MINING_REWARD
     # }
     reward_transaction = OrderedDict(
-        [('sender','MINING'),('recipient', owner),('amount', MINING_REWARD)]
+        [('sender', 'MINING'), ('recipient', owner), ('amount', MINING_REWARD)]
     )
     copied_transaction = open_transactions[:]
     copied_transaction.append(reward_transaction)
@@ -104,6 +151,7 @@ def mine_block():
         'proof': proof
     }
     blockchain.append(block)
+    
 
 
 def get_transaction_value():
@@ -162,6 +210,7 @@ while waiting_for_input:
     elif user_choice == '2':
         if mine_block():
             open_transactions = []
+            save_data()
     elif user_choice == '3':
         print_blockchain_elements()
     elif user_choice == '4':
